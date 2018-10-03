acbe241a2165a5f900610b71d16b55990a110bec
Calvin French <calvin@kittylambda.com>
2018-10-03 01:03:59 -0600

    - renamed source/dest to local/remote in tests

--- test_sync.rb
+++ test_sync.rb
@@ -11,20 +11,23 @@ class SyncTester
   TEMP_DIR="#{__dir__}/temp"
 
   def initialize
-    #test_dot_sync_dir_was_initialized
-    #test_up_sync_into_empty_dir
+    test_dot_sync_dir_was_initialized
+    test_up_sync_into_empty_dir
     test_up_sync_file_changes_locally
-    #test_down_sync_into_empty_dir_folders
-    #test_down_sync_file_changes_on_server
+    test_up_sync_immediate_change_to_new_file_from_down_sync
+    test_down_sync_into_empty_dir_folders
+    test_down_sync_file_changes_on_server
+
     # test down sync, file deleted on server, down sync, file not deleted, enable rsync delete, down sync, file deleted
-    # test down sync, file deleted locall, down sync, file present again
+    # test down sync, file deleted local, down sync, file present again
     # test above in fast and full modes
+    puts
   end
 
   def test_dot_sync_dir_was_initialized
-    source_dir, dest_dir = _setup(false, false)
-    _assert_dir_contents "#{source_dir}/.sync", ['sync_settings.txt'], 'sync_settings.txt is in .sync dir'
-    settings = _load_sync_settings(source_dir)
+    local_dir, remote_dir = _setup(false, false)
+    _assert_dir_contents "#{local_dir}/.sync", ['sync_settings.txt'], 'sync_settings.txt is in .sync dir'
+    settings = _load_sync_settings(local_dir)
     _assert_equals settings['rsync_delete'], false, 'rsync_delete is initially set to false'
     _assert_equals settings['rsync_dry_run'], true, 'rsync_dry_run is initially set to false'
     _assert_equals settings['fast_mode'], false, 'fast_mode is initially set to false'
@@ -32,66 +35,77 @@ class SyncTester
   end
 
   def test_up_sync_into_empty_dir
-    source_dir, dest_dir = _setup(true, false)
-    _setup_settings(source_dir, dest_dir)
-    _assert_dir_contents dest_dir, [], 'dest dir is empty before sync'
+    local_dir, remote_dir = _setup(true, false)
+    _setup_settings(local_dir, remote_dir)
+    _assert_dir_contents remote_dir, [], 'dest dir is empty before sync'
     `./sync.rb`
-    _assert_dirs_match dest_dir, source_dir, 'dest dir matches source dir after sync'
+    _assert_dirs_match remote_dir, local_dir, 'dest dir matches source dir after sync'
   end
 
   def test_up_sync_file_changes_locally
-    source_dir, dest_dir = _setup(true, true)
-    _setup_settings(source_dir, dest_dir)
+    local_dir, remote_dir = _setup(true, false)
+    _setup_settings(local_dir, remote_dir)
     `./sync.rb`
-    _assert_file_contents "#{dest_dir}/TESTING/hello.txt", "hello there\n", 'file has expected contents after initial up sync'
-    _set_file_contents "#{source_dir}/TESTING/hello.txt", "hello again\n"
+    _assert_file_contents "#{remote_dir}/TESTING/hello.txt", "hello there\n", 'file has expected contents after initial up sync'
+    _set_file_contents "#{local_dir}/TESTING/hello.txt", "hello again\n"
     `./sync.rb`
-    _assert_file_contents "#{dest_dir}/TESTING/hello.txt", "hello again\n", 'file has new contents after second up sync'
+    _assert_file_contents "#{remote_dir}/TESTING/hello.txt", "hello again\n", 'file has new contents after second up sync'
+  end
+
+  def test_up_sync_immediate_change_to_new_file_from_down_sync
+    local_dir, remote_dir = _setup(false, true)
+    _setup_settings(local_dir, remote_dir)
+    `mkdir #{local_dir}/TESTING`
+    `./sync.rb`
+    _assert_file_contents "#{local_dir}/TESTING/hello.txt", "hello there\n", 'file has expected contents after initial down sync'
+    _set_file_contents "#{local_dir}/TESTING/hello.txt", "hello immediately\n"
+    `./sync.rb`
+    _assert_file_contents "#{remote_dir}/TESTING/hello.txt", "hello immediately\n", 'dest file has new contents after second sync'
   end
 
   def test_down_sync_into_empty_dir_folders
-    source_dir, dest_dir = _setup(false, true)
-    _setup_settings(source_dir, dest_dir)
-    _assert_dir_contents source_dir, ['sync.rb'], 'source dir is empty before sync'
+    local_dir, remote_dir = _setup(false, true)
+    _setup_settings(local_dir, remote_dir)
+    _assert_dir_contents local_dir, ['sync.rb'], 'source dir is empty before sync'
     `./sync.rb`
-    _assert_dir_contents source_dir, ['sync.rb'], 'source dir is empty after down sync into empty dir'
-    `mkdir #{source_dir}/TESTING`
-    `mkdir #{source_dir}/TESTING_2`
+    _assert_dir_contents local_dir, ['sync.rb'], 'source dir is empty after down sync into empty dir'
+    `mkdir #{local_dir}/TESTING`
+    `mkdir #{local_dir}/TESTING_2`
     `./sync.rb`
-    _assert_dirs_match source_dir, dest_dir, 'source dir matches dest dir after sync with folder created'
+    _assert_dirs_match local_dir, remote_dir, 'source dir matches dest dir after sync with folder created'
   end
 
   def test_down_sync_file_changes_on_server
-    source_dir, dest_dir = _setup(false, true)
-    _setup_settings(source_dir, dest_dir)
-    `mkdir #{source_dir}/TESTING`
+    local_dir, remote_dir = _setup(false, true)
+    _setup_settings(local_dir, remote_dir)
+    `mkdir #{local_dir}/TESTING`
     `./sync.rb`
-    _assert_file_contents "#{dest_dir}/TESTING/hello.txt", "hello there\n", 'hello.txt has expected contents after initial down sync'
-    _set_file_contents "#{source_dir}/TESTING/hello.txt", "hello again\n"
+    _assert_file_contents "#{remote_dir}/TESTING/hello.txt", "hello there\n", 'hello.txt has expected contents after initial down sync'
+    _set_file_contents "#{local_dir}/TESTING/hello.txt", "hello again\n"
     `./sync.rb`
-    _assert_file_contents "#{dest_dir}/TESTING/hello.txt", "hello again\n", 'hello.txt has new contents after second down sync'
+    _assert_file_contents "#{remote_dir}/TESTING/hello.txt", "hello again\n", 'hello.txt has new contents after second down sync'
   end
 
   private
 
-  def _setup(init_source_dir_contents, init_dest_dir_contents)
+  def _setup(init_local_dir_contents, init_remote_dir_contents)
     Dir.mkdir(TEMP_DIR) unless Dir.exist?(TEMP_DIR)
-    source_dir = _cleanpath Dir.mktmpdir('source_', TEMP_DIR)
-    dest_dir = _cleanpath Dir.mktmpdir('dest_', TEMP_DIR)
+    local_dir = _cleanpath Dir.mktmpdir('source_', TEMP_DIR)
+    remote_dir = _cleanpath Dir.mktmpdir('dest_', TEMP_DIR)
 
-    _assert_dir_contents_size source_dir, 0, 'source dir is empty before setup'
-    _setup_dir_contents source_dir, 'source dir' if init_source_dir_contents
+    _assert_dir_contents_size local_dir, 0, 'source dir is empty before setup'
+    _setup_dir_contents local_dir, 'source dir' if init_local_dir_contents
 
-    _assert_dir_contents_size dest_dir, 0, 'dest dir is empty before setup'
-    _setup_dir_contents dest_dir, 'dest dir' if init_dest_dir_contents
+    _assert_dir_contents_size remote_dir, 0, 'dest dir is empty before setup'
+    _setup_dir_contents remote_dir, 'dest dir' if init_remote_dir_contents
 
-    `cp #{Shellwords.escape(SYNC_RB)} #{Shellwords.escape(source_dir)}`
-    Dir.chdir(source_dir)
-    # _assert_equals source_dir, _cleanpath(Dir.getwd), 'changed working dir to source testing dir'
+    `cp #{Shellwords.escape(SYNC_RB)} #{Shellwords.escape(local_dir)}`
+    Dir.chdir(local_dir)
+    # _assert_equals local_dir, _cleanpath(Dir.getwd), 'changed working dir to source testing dir'
     sync_output = `./sync.rb`
     _assert_string_match sync_output, 'ERROR: please edit .sync/sync_settings.txt', 'first-run .sync dir setup'
 
-    return source_dir, dest_dir
+    return local_dir, remote_dir
   end
 
   def _setup_dir_contents(dir, dir_desc)
@@ -101,13 +115,13 @@ class SyncTester
     _assert_dir_contents_size "#{dir}/TESTING", 6, "#{dir_desc} has expected number of files in TESTING/ after setup"
   end
 
-  def _setup_settings(source_dir, dest_dir)
-    settings = _load_sync_settings(source_dir)
-    settings['upstream_folder'] = "#{dest_dir}"
+  def _setup_settings(local_dir, remote_dir)
+    settings = _load_sync_settings(local_dir)
+    settings['upstream_folder'] = "#{remote_dir}"
     settings['sleep_time'] = 0
     settings['rsync_dry_run'] = false
     settings['settings_are_set'] = true
-    _save_sync_settings(source_dir, settings)
+    _save_sync_settings(local_dir, settings)
   end
 
   def _assert_equals(actual, expected, desc)
@@ -166,16 +180,16 @@ class SyncTester
     File.expand_path(dir, '/')
   end
 
-  def _sync_settings_filename(source_dir)
-    "#{source_dir}/.sync/sync_settings.txt"
+  def _sync_settings_filename(local_dir)
+    "#{local_dir}/.sync/sync_settings.txt"
   end
 
-  def _load_sync_settings(source_dir)
-    YAML.load_file(_sync_settings_filename(source_dir))
+  def _load_sync_settings(local_dir)
+    YAML.load_file(_sync_settings_filename(local_dir))
   end
 
-  def _save_sync_settings(source_dir, settings)
-    File.write(_sync_settings_filename(source_dir), settings.to_yaml)
+  def _save_sync_settings(local_dir, settings)
+    File.write(_sync_settings_filename(local_dir), settings.to_yaml)
   end
 end
 
