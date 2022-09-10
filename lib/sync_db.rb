c1e9d0f96a64d89d41ae4f4d2349f588bd0a2889
Calvin French <calvin@kittylambda.com>
2022-09-10 14:25:06 -0600

    - convert indentation to tabs (globally) (ugh)

--- lib/sync_db.rb
+++ lib/sync_db.rb
@@ -2,191 +2,191 @@
 
 # helper class; this represents a file information database, for storing shas and/or file sizes for a single root folder
 class FileSyncDB
-  def initialize(folder_name)
-    @folder_name = folder_name
-    @file_info_filename = "#{DOT_SYNC_FOLDER}/#{folder_name}_info.yaml"
-    @file_info = {} # map filename => { 'sync_ts' => timestamp, 'sha256' => sha256 }
+	def initialize(folder_name)
+		@folder_name = folder_name
+		@file_info_filename = "#{DOT_SYNC_FOLDER}/#{folder_name}_info.yaml"
+		@file_info = {} # map filename => { 'sync_ts' => timestamp, 'sha256' => sha256 }
 
-    # if the info file exists, load it
-    begin
-      _validate_and_set_file_info(YAML.load_file(@file_info_filename)) if File.exist?(@file_info_filename)
-    rescue StandardError => e
-      puts "💀  ERROR: could not load file info from #{@file_info_filename}"
-      puts e
-      exit(-1)
-    end
-  end
+		# if the info file exists, load it
+		begin
+			_validate_and_set_file_info(YAML.load_file(@file_info_filename)) if File.exist?(@file_info_filename)
+		rescue StandardError => e
+			puts "💀  ERROR: could not load file info from #{@file_info_filename}"
+			puts e
+			exit(-1)
+		end
+	end
 
-  def save_file_info
-    dest_file = Tempfile.new('file_sync_db')
-    dest_file.write(@file_info.to_yaml)
-    dest_file.close
-    FileUtils.mv(dest_file.path, @file_info_filename)
-  rescue StandardError => e
-    puts "💀  ERROR: could not save file sync db to #{@file_info_filename}"
-    puts e
-    exit(-1)
-  end
+	def save_file_info
+		dest_file = Tempfile.new('file_sync_db')
+		dest_file.write(@file_info.to_yaml)
+		dest_file.close
+		FileUtils.mv(dest_file.path, @file_info_filename)
+	rescue StandardError => e
+		puts "💀  ERROR: could not save file sync db to #{@file_info_filename}"
+		puts e
+		exit(-1)
+	end
 
-  # determine a list of files to update, and update our file info accordingly so it can be saved for the next run
-  # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/MethodLength
-  def update_file_info_and_find_all_files_to_up_sync!(puts_prefix, start_sync_ts)
-    # get a list of everything in this folder, and select only those that need to be sync'd
-    all_file_stats = {}
-    _find_all_file_stats(@folder_name, all_file_stats)
+	# determine a list of files to update, and update our file info accordingly so it can be saved for the next run
+	# rubocop:disable Metrics/AbcSize
+	# rubocop:disable Metrics/MethodLength
+	def update_file_info_and_find_all_files_to_up_sync!(puts_prefix, start_sync_ts)
+		# get a list of everything in this folder, and select only those that need to be sync'd
+		all_file_stats = {}
+		_find_all_file_stats(@folder_name, all_file_stats)
 
-    # calculate shas for the files we need them for
-    all_shas = _get_required_file_shas(puts_prefix, all_file_stats)
+		# calculate shas for the files we need them for
+		all_shas = _get_required_file_shas(puts_prefix, all_file_stats)
 
-    # compute anything that needs to be up-sync'd
-    # - if we don't have a record for it
-    # - if we have a sha, but the sha record doesn't match (or isn't present)
-    # - if we have a timestamp, and the timestamp is newer than the record
-    sync_filenames = []
-    all_file_stats.each do |filename, stats|
-      file_info_line = @file_info[filename]
-      sha256 = all_shas[filename]
+		# compute anything that needs to be up-sync'd
+		# - if we don't have a record for it
+		# - if we have a sha, but the sha record doesn't match (or isn't present)
+		# - if we have a timestamp, and the timestamp is newer than the record
+		sync_filenames = []
+		all_file_stats.each do |filename, stats|
+			file_info_line = @file_info[filename]
+			sha256 = all_shas[filename]
 
-      req_sync =
-        if !file_info_line
-          # we don't have a file info line yet for this file
-          true
-        elsif sha256
-          # we have a sha256, so only update if it doesn't match what we have in our file info
-          sha256 != file_info_line['sha256']
-        else
-          stats['mtime'] > file_info_line['sync_ts']
-        end
-      sync_filenames << filename if req_sync
-    end
+			req_sync =
+				if !file_info_line
+					# we don't have a file info line yet for this file
+					true
+				elsif sha256
+					# we have a sha256, so only update if it doesn't match what we have in our file info
+					sha256 != file_info_line['sha256']
+				else
+					stats['mtime'] > file_info_line['sync_ts']
+				end
+			sync_filenames << filename if req_sync
+		end
 
-    # update our file info; update sync time to be now, and sha256 if we have it
-    all_file_stats.each do |filename, _stats|
-      file_info_line = @file_info[filename]
-      unless file_info_line
-        @file_info[filename] = {}
-        file_info_line = @file_info[filename]
-      end
+		# update our file info; update sync time to be now, and sha256 if we have it
+		all_file_stats.each do |filename, _stats|
+			file_info_line = @file_info[filename]
+			unless file_info_line
+				@file_info[filename] = {}
+				file_info_line = @file_info[filename]
+			end
 
-      # update our sha256
-      # - note this will clear out shas that might have been there before
-      # - this is desired; it will eliminate stale shas even if stale because we changed the limit
-      file_info_line['sha256'] = all_shas[filename]
+			# update our sha256
+			# - note this will clear out shas that might have been there before
+			# - this is desired; it will eliminate stale shas even if stale because we changed the limit
+			file_info_line['sha256'] = all_shas[filename]
 
-      # update our sync time
-      file_info_line['sync_ts'] = start_sync_ts
-    end
+			# update our sync time
+			file_info_line['sync_ts'] = start_sync_ts
+		end
 
-    sync_filenames
-  end
-  # rubocop:enable Metrics/AbcSize
-  # rubocop:enable Metrics/MethodLength
+		sync_filenames
+	end
+	# rubocop:enable Metrics/AbcSize
+	# rubocop:enable Metrics/MethodLength
 
-  # refresh everything for files newer than the given timestamp
-  # - this is intended for use after down sync, to update the file sync db with any downloaded files
-  # rubocop:disable Metrics/AbcSize
-  def update_file_info_after_down_sync!(puts_prefix, start_sync_ts, end_sync_ts)
-    all_file_stats = {}
-    _find_all_file_stats(@folder_name, all_file_stats)
+	# refresh everything for files newer than the given timestamp
+	# - this is intended for use after down sync, to update the file sync db with any downloaded files
+	# rubocop:disable Metrics/AbcSize
+	def update_file_info_after_down_sync!(puts_prefix, start_sync_ts, end_sync_ts)
+		all_file_stats = {}
+		_find_all_file_stats(@folder_name, all_file_stats)
 
-    # choose only the newly downloaded or updated files
-    # - note we use rsync --times, which will preserve mtime; so, check atime instead which is the actual time the file was created
-    # - we could also scan rsync output itself
-    new_file_stats = {}
-    all_file_stats.each do |filename, stats|
-      new_file_stats[filename] = stats if stats['atime'] >= start_sync_ts
-    end
+		# choose only the newly downloaded or updated files
+		# - note we use rsync --times, which will preserve mtime; so, check atime instead which is the actual time the file was created
+		# - we could also scan rsync output itself
+		new_file_stats = {}
+		all_file_stats.each do |filename, stats|
+			new_file_stats[filename] = stats if stats['atime'] >= start_sync_ts
+		end
 
-    # calculate shas for new files, as needed, as with up-sync
-    all_shas = _get_required_file_shas(puts_prefix, new_file_stats)
+		# calculate shas for new files, as needed, as with up-sync
+		all_shas = _get_required_file_shas(puts_prefix, new_file_stats)
 
-    # update file infos for all the ewer files
-    new_file_stats.each do |filename, _stats|
-      file_info_line = @file_info[filename]
-      unless file_info_line
-        @file_info[filename] = {}
-        file_info_line = @file_info[filename]
-      end
+		# update file infos for all the ewer files
+		new_file_stats.each do |filename, _stats|
+			file_info_line = @file_info[filename]
+			unless file_info_line
+				@file_info[filename] = {}
+				file_info_line = @file_info[filename]
+			end
 
-      # set sha2456, as with up-sync
-      file_info_line['sha256'] = all_shas[filename]
+			# set sha2456, as with up-sync
+			file_info_line['sha256'] = all_shas[filename]
 
-      # set the sync ts to the file update ts; set the update ts, not the sync ts, as update will be slightly later
-      file_info_line['sync_ts'] = end_sync_ts # stats['update_ts']
-    end
-  end
-  # rubocop:enable Metrics/AbcSize
+			# set the sync ts to the file update ts; set the update ts, not the sync ts, as update will be slightly later
+			file_info_line['sync_ts'] = end_sync_ts # stats['update_ts']
+		end
+	end
+	# rubocop:enable Metrics/AbcSize
 
-  private
+	private
 
-  # set the @file_info array from file_info, validating it first
-  # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/CyclomaticComplexity
-  def _validate_and_set_file_info(file_info)
-    # check that file_info is a hash
-    raise StandardError, 'loaded file info is not a hash' unless file_info.is_a?(Hash)
+	# set the @file_info array from file_info, validating it first
+	# rubocop:disable Metrics/AbcSize
+	# rubocop:disable Metrics/CyclomaticComplexity
+	def _validate_and_set_file_info(file_info)
+		# check that file_info is a hash
+		raise StandardError, 'loaded file info is not a hash' unless file_info.is_a?(Hash)
 
-    # check that each element is a hash, keyed on filename in this folder, and with no unexpected fields
-    valid_info_keys = %w[sync_ts sha256]
-    file_info.each do |filename, info|
-      raise StandardError, "loaded file info contains an invalid key #{filename}" unless filename.is_a?(String)
-      unless filename.start_with?("#{@folder_name}/") && Pathname.new(filename).cleanpath.to_s == filename
-        raise StandardError, "loaded file info contains a filename #{filename} not in the expected path #{@folder_name}"
-      end
+		# check that each element is a hash, keyed on filename in this folder, and with no unexpected fields
+		valid_info_keys = %w[sync_ts sha256]
+		file_info.each do |filename, info|
+			raise StandardError, "loaded file info contains an invalid key #{filename}" unless filename.is_a?(String)
+			unless filename.start_with?("#{@folder_name}/") && Pathname.new(filename).cleanpath.to_s == filename
+				raise StandardError, "loaded file info contains a filename #{filename} not in the expected path #{@folder_name}"
+			end
 
-      raise StandardError, 'loaded file info contains a line that is not a hash' unless info.is_a?(Hash)
+			raise StandardError, 'loaded file info contains a line that is not a hash' unless info.is_a?(Hash)
 
-      invalid_info_keys = info.keys - valid_info_keys
-      unless invalid_info_keys.empty?
-        raise StandardError, "loaded file info contains a line with one or more invalid keys: #{invalid_info_keys}"
-      end
-    end
+			invalid_info_keys = info.keys - valid_info_keys
+			unless invalid_info_keys.empty?
+				raise StandardError, "loaded file info contains a line with one or more invalid keys: #{invalid_info_keys}"
+			end
+		end
 
-    # made it this far, then everything is good
-    @file_info = file_info
-  end
-  # rubocop:enable Metrics/AbcSize
-  # rubocop:enable Metrics/CyclomaticComplexity
+		# made it this far, then everything is good
+		@file_info = file_info
+	end
+	# rubocop:enable Metrics/AbcSize
+	# rubocop:enable Metrics/CyclomaticComplexity
 
-  # find all files, starting from the given folder, not including dotfiles
-  # @param dest_file_stats [Hash<String, Hash>] output map from filename => { 'size' => size, 'mtime' => mtime, 'atime' => atime }
-  def _find_all_file_stats(folder_name, dest_file_stats)
-    Dir.glob("#{folder_name}/*") do |f|
-      if File.basename(f).start_with?('.')
-        # nothing
-      elsif File.directory?(f)
-        _find_all_file_stats(f, dest_file_stats)
-      elsif File.file?(f)
-        fs = File.stat(f)
-        raise StandardError, "could not stat file: #{f}" unless fs
+	# find all files, starting from the given folder, not including dotfiles
+	# @param dest_file_stats [Hash<String, Hash>] output map from filename => { 'size' => size, 'mtime' => mtime, 'atime' => atime }
+	def _find_all_file_stats(folder_name, dest_file_stats)
+		Dir.glob("#{folder_name}/*") do |f|
+			if File.basename(f).start_with?('.')
+				# nothing
+			elsif File.directory?(f)
+				_find_all_file_stats(f, dest_file_stats)
+			elsif File.file?(f)
+				fs = File.stat(f)
+				raise StandardError, "could not stat file: #{f}" unless fs
 
-        dest_file_stats[f] = {
-          'size' => fs.size,
-          'mtime' => fs.mtime.to_i,
-          'atime' => fs.atime.to_i
-        }
-      else
-        print "💀  WARNING: not adding file #{f}"
-      end
-    end
-  end
+				dest_file_stats[f] = {
+					'size' => fs.size,
+					'mtime' => fs.mtime.to_i,
+					'atime' => fs.atime.to_i
+				}
+			else
+				print "💀  WARNING: not adding file #{f}"
+			end
+		end
+	end
 
-  # calculate all shas for required files, given an input file stats map
-  # - this depends on the include/exclude root folders
-  def _get_required_file_shas(puts_prefix, all_file_stats)
-    is_root_folder_included = FAST_MODE_INCLUDE_ROOT_FOLDERS.empty? || FAST_MODE_INCLUDE_ROOT_FOLDERS.include?(@folder_name)
-    is_root_folder_excluded = FAST_MODE_EXCLUDE_ROOT_FOLDERS.include?(@folder_name)
-    use_fast_mode = FAST_MODE && is_root_folder_included && !is_root_folder_excluded
-    all_sha_filenames =
-      if use_fast_mode
-        puts "#{puts_prefix}: ! skipping sha calculations for folder #{@folder_name}"
-        []
-      else
-        puts "#{puts_prefix}: ! computing full sha signatures for folder #{@folder_name}"
-        all_file_stats.keys
-      end
+	# calculate all shas for required files, given an input file stats map
+	# - this depends on the include/exclude root folders
+	def _get_required_file_shas(puts_prefix, all_file_stats)
+		is_root_folder_included = FAST_MODE_INCLUDE_ROOT_FOLDERS.empty? || FAST_MODE_INCLUDE_ROOT_FOLDERS.include?(@folder_name)
+		is_root_folder_excluded = FAST_MODE_EXCLUDE_ROOT_FOLDERS.include?(@folder_name)
+		use_fast_mode = FAST_MODE && is_root_folder_included && !is_root_folder_excluded
+		all_sha_filenames =
+			if use_fast_mode
+				puts "#{puts_prefix}: ! skipping sha calculations for folder #{@folder_name}"
+				[]
+			else
+				puts "#{puts_prefix}: ! computing full sha signatures for folder #{@folder_name}"
+				all_file_stats.keys
+			end
 
-    get_file_shas(puts_prefix, all_sha_filenames)
-  end
+		get_file_shas(puts_prefix, all_sha_filenames)
+	end
 end
--- lib/sync_settings.rb
+++ lib/sync_settings.rb
@@ -5,144 +5,144 @@ require 'yaml'
 
 # Load/save sync settings from the .sync folder
 class SyncSettings
-  DOT_SYNC_FOLDER = '.sync'
-  SYNC_SETTINGS_BASENAME = 'sync_settings.txt'
-  SYNC_SETTINGS_FILENAME = "#{DOT_SYNC_FOLDER}/#{SYNC_SETTINGS_BASENAME}"
-  SHASUM_BIN = 'shasum -b'
+	DOT_SYNC_FOLDER = '.sync'
+	SYNC_SETTINGS_BASENAME = 'sync_settings.txt'
+	SYNC_SETTINGS_FILENAME = "#{DOT_SYNC_FOLDER}/#{SYNC_SETTINGS_BASENAME}"
+	SHASUM_BIN = 'shasum -b'
 
-  def initialize
-    _check_create_dot_sync_folder
-    _load_settings
-    _validate_settings
-  end
+	def initialize
+		_check_create_dot_sync_folder
+		_load_settings
+		_validate_settings
+	end
 
-  def upsteam_folder
-    @settings['upstream_folder'].to_s
-  end
+	def upsteam_folder
+		@settings['upstream_folder'].to_s
+	end
 
-  def sleep_time
-    @settings['sleep_time'].to_i
-  end
+	def sleep_time
+		@settings['sleep_time'].to_i
+	end
 
-  def rsync_dry_run
-    @settings['rsync_dry_run'] ? '-n' : ''
-  end
+	def rsync_dry_run
+		@settings['rsync_dry_run'] ? '-n' : ''
+	end
 
-  def rsync_delete
-    @settings['rsync_delete'] ? '--delete' : ''
-  end
+	def rsync_delete
+		@settings['rsync_delete'] ? '--delete' : ''
+	end
 
-  def rsync_progress
-    @settings['rsync_progress'] ? '--progress' : ''
-  end
+	def rsync_progress
+		@settings['rsync_progress'] ? '--progress' : ''
+	end
 
-  def fast_mode?
-    @settings['fast_mode']
-  end
+	def fast_mode?
+		@settings['fast_mode']
+	end
 
-  def fast_mode_include_root_folders
-    @settings['fast_mode_include_root_folders']
-  end
+	def fast_mode_include_root_folders
+		@settings['fast_mode_include_root_folders']
+	end
 
-  def fast_mode_exclude_root_folders
-    @settings['fast_mode_exclude_root_folders']
-  end
+	def fast_mode_exclude_root_folders
+		@settings['fast_mode_exclude_root_folders']
+	end
 
-  private
+	private
 
-  # rubocop:disable Metrics/AbcSize
-  def _validate_setings
-    # make sure settings file was actually initialized
-    unless _settings_are_set
-      puts "💀  ERROR: please edit #{SYNC_SETTINGS_FILENAME} with your sync settings, and set settings_are_set to Yes"
-      exit(-1)
-    end
+	# rubocop:disable Metrics/AbcSize
+	def _validate_setings
+		# make sure settings file was actually initialized
+		unless _settings_are_set
+			puts "💀  ERROR: please edit #{SYNC_SETTINGS_FILENAME} with your sync settings, and set settings_are_set to Yes"
+			exit(-1)
+		end
 
-    # make sure they specified an upstream folder
-    unless upstream_folder && !_upstream_folder.empty?
-      puts "💀  ERROR: please specify upstream_folder in #{SYNC_SETTINGS_FILENAME}"
-      exit(-1)
-    end
+		# make sure they specified an upstream folder
+		unless upstream_folder && !_upstream_folder.empty?
+			puts "💀  ERROR: please specify upstream_folder in #{SYNC_SETTINGS_FILENAME}"
+			exit(-1)
+		end
 
-    # make sure they specified a valid sleep time
-    unless sleep_time && sleep_time >= 0
-      puts "💀  ERROR: please specify sleep_time >= 0 in #{SYNC_SETTINGS_FILENAME}"
-      exit(-1)
-    end
+		# make sure they specified a valid sleep time
+		unless sleep_time && sleep_time >= 0
+			puts "💀  ERROR: please specify sleep_time >= 0 in #{SYNC_SETTINGS_FILENAME}"
+			exit(-1)
+		end
 
-    # print a warning if we're in dry run mode
-    puts "💀  WARNING: executing in dry-run mode; not actually sync'ing anything" unless rsync_dry_run.empty?
-  end
-  # rubocop:enable Metrics/AbcSize
+		# print a warning if we're in dry run mode
+		puts "💀  WARNING: executing in dry-run mode; not actually sync'ing anything" unless rsync_dry_run.empty?
+	end
+	# rubocop:enable Metrics/AbcSize
 
-  def _settings_are_set
-    @settings && @settings['settings_are_set'] == true
-  end
+	def _settings_are_set
+		@settings && @settings['settings_are_set'] == true
+	end
 
-  def _load_settings
-    # try to load the sync_settings.txt (YAML)
-    @settings = YAML.load_file(SYNC_SETTINGS_FILENAME)
-  rescue StandardError => e
-    puts "💀  ERROR: could not load settings from #{SYNC_SETTINGS_FILENAME}"
-    puts e
-    exit(-1)
-  end
+	def _load_settings
+		# try to load the sync_settings.txt (YAML)
+		@settings = YAML.load_file(SYNC_SETTINGS_FILENAME)
+	rescue StandardError => e
+		puts "💀  ERROR: could not load settings from #{SYNC_SETTINGS_FILENAME}"
+		puts e
+		exit(-1)
+	end
 
-  def _check_create_dot_sync_folder
-    return if File.directory?(DOT_SYNC_FOLDER)
+	def _check_create_dot_sync_folder
+		return if File.directory?(DOT_SYNC_FOLDER)
 
-    puts "creating #{DOT_SYNC_FOLDER}"
-    Dir.mkdir(DOT_SYNC_FOLDER)
-    File.open(SYNC_SETTINGS_FILENAME, 'w') do |f|
-      f.write <<~END_OF_SYNC_SETTINGS_STUB
-        # This file controls sync settings. It must be edited before sync will work.
+		puts "creating #{DOT_SYNC_FOLDER}"
+		Dir.mkdir(DOT_SYNC_FOLDER)
+		File.open(SYNC_SETTINGS_FILENAME, 'w') do |f|
+			f.write <<~END_OF_SYNC_SETTINGS_STUB
+				# This file controls sync settings. It must be edited before sync will work.
 
-        # this defines the server you sync to
-        # - set it to an rsync-able path to sync to, generally an ssh-able path
-        # - every node you create should sync to the same upstream
-        upstream_folder: "user@example.com:/Path/to/archive"
+				# this defines the server you sync to
+				# - set it to an rsync-able path to sync to, generally an ssh-able path
+				# - every node you create should sync to the same upstream
+				upstream_folder: "user@example.com:/Path/to/archive"
 
-        # this controls the delay between syncing servers, to avoid your server detecting a connection flood
-        # - set it to a larger value if you see connection failures
-        # - set it to a smaller value if you think you can get away with it
-        sleep_time: 2
+				# this controls the delay between syncing servers, to avoid your server detecting a connection flood
+				# - set it to a larger value if you see connection failures
+				# - set it to a smaller value if you think you can get away with it
+				sleep_time: 2
 
-        # this determines whether rsync will be allowed to delete files locally
-        # - if set, moving files on the server will cause them to be moved locally on sync
-        # - if unset, moving files on the server will cause them to be duplicated
-        rsync_delete: No
+				# this determines whether rsync will be allowed to delete files locally
+				# - if set, moving files on the server will cause them to be moved locally on sync
+				# - if unset, moving files on the server will cause them to be duplicated
+				rsync_delete: No
 
-        # this will prevent any files actually being transferred
-        # - set it it to No once you've done a test run or two and think things are probably OK
-        # - useful for test runs
-        rsync_dry_run: Yes
+				# this will prevent any files actually being transferred
+				# - set it it to No once you've done a test run or two and think things are probably OK
+				# - useful for test runs
+				rsync_dry_run: Yes
 
-        # this controls whether or not rsync displays progress while syncing
-        # - set it to No to squelch progress messages if they annoy you
-        # - progress messages may work better or worse on some platforms (i.e., worse on Windows)
-        rsync_progress: Yes
+				# this controls whether or not rsync displays progress while syncing
+				# - set it to No to squelch progress messages if they annoy you
+				# - progress messages may work better or worse on some platforms (i.e., worse on Windows)
+				rsync_progress: Yes
 
-        # this controls whether or not to allow fast mode
-        # - fast mode works by checking modification time only, and only for files larger than some limit
-        # - if turned on, the fast mode limit will be in effect
-        fast_mode: No
+				# this controls whether or not to allow fast mode
+				# - fast mode works by checking modification time only, and only for files larger than some limit
+				# - if turned on, the fast mode limit will be in effect
+				fast_mode: No
 
-        # if fast mode is enabled, this can be used to specify included and excluded root folders
-        # - fast_mode_include_root_folders contains the folders to sync using fast mode; if empty, all will be synced this way
-        # - fast_mode_exclude_root_folders contains any folders to NOT sync using fast mode; overrides fast_mode_include_root_folders
-        # - only valid for root folders
-        # - e.g., [ "my_folder", "my_other_folder" ]
-        fast_mode_include_root_folders: []
-        fast_mode_exclude_root_folders: []
+				# if fast mode is enabled, this can be used to specify included and excluded root folders
+				# - fast_mode_include_root_folders contains the folders to sync using fast mode; if empty, all will be synced this way
+				# - fast_mode_exclude_root_folders contains any folders to NOT sync using fast mode; overrides fast_mode_include_root_folders
+				# - only valid for root folders
+				# - e.g., [ "my_folder", "my_other_folder" ]
+				fast_mode_include_root_folders: []
+				fast_mode_exclude_root_folders: []
 
-        # this tells sync.rb that you have configured this file
-        # - set it to Yes once you've configured this file
-        settings_are_set: No
-      END_OF_SYNC_SETTINGS_STUB
-    rescue StandardError => e
-      puts "💀  ERROR: could not create #{DOT_SYNC_FOLDER} or #{SYNC_SETTINGS_FILENAME}"
-      puts e
-      exit(-1)
-    end
-  end
+				# this tells sync.rb that you have configured this file
+				# - set it to Yes once you've configured this file
+				settings_are_set: No
+			END_OF_SYNC_SETTINGS_STUB
+		rescue StandardError => e
+			puts "💀  ERROR: could not create #{DOT_SYNC_FOLDER} or #{SYNC_SETTINGS_FILENAME}"
+			puts e
+			exit(-1)
+		end
+	end
 end
